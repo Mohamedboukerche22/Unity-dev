e874e1c61d3bc45edd92ef74d859a4b7e80cee6f
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-09-15 01:16:40 +0100

    Update playermovement.cs

--- 3d-charcter-controller/playermovement.cs
+++ 3d-charcter-controller/playermovement.cs
@@ -14,6 +14,7 @@ public class Playermovement : MonoBehaviour
    Animator anim;
     void Start()
     {
+    Cursor.visible = false;
        
         controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
